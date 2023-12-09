5a68ded91d7f026d8e1131d2b456a3fffd320f48
Aiden Pinto <aidenpinto14@gmail.com>
2023-12-09 13:27:33 -0500

    Added some youtube search code

---
+++ youtube_search.py
@@ -0,0 +1,20 @@
+from youtubesearchpython import VideosSearch
+
+def search_youtube(keyword):
+    try:
+        video_search = VideosSearch(keyword, limit = 1)
+        results = video_search.result()
+        if results["result"]:
+            top_result = results["result"][0]
+            video_id = top_result["id"]
+            youtube_link = f"https://www.youtube.com/watch?v={video_id}"
+            print("Top result on YouTube:", youtube_link)
+            return youtube_link
+        else:
+            print("No results found on YouTube.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Example usage
+search_keyword = input("Enter the keyword to search on YouTube: ")
+search_youtube(search_keyword)
